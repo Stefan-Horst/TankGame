b58b14a0463020a10157c477ed957ddb6c277f76
Stefan <stefan-horst@gmx.de>
2022-11-29 06:16:36 +0100

    Refactor project structure

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -6,7 +6,10 @@ var backwards_steering_mode = 0
 var speed = 200
 var angular_speed = 1
 
+onready var turret = $SpriteTurret
+
 func _process(delta):
+	# player movement
 	var direction = 0
 	if Input.is_action_pressed("ui_left"):
 		direction = -1
@@ -23,3 +26,7 @@ func _process(delta):
 
 	rotation += angular_speed * direction * delta
 	position += velocity * delta
+	
+	# player turret alway point at cursor
+	turret.look_at(get_global_mouse_position())
+	turret.rotation_degrees += 90 #either this fix or rotate sprite image
--- entities/player/turret.gd
+++
@@ -1,6 +0,0 @@
-extends Sprite
-
-
-func _process(_delta):
-	look_at(get_global_mouse_position())
-	rotation_degrees += 90
