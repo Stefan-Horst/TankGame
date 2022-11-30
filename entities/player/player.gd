59537883122883439f1fa7152e31c4e05256727f
ChristopherOehring <chris.oehring@gmx.de>
2022-11-30 15:17:33 +0100

    Add Player Collision
    Moved player movement to _physics_process for accurate computation.
    Now uses move_and_slide which adds collision and sliding along walls.

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -8,7 +8,9 @@ var angular_speed = 1
 
 onready var turret = $SpriteTurret
 
-func _process(delta):
+# _physics_process is computet in fixed intervals, which is required for accurate
+# computation of move_and_slide()
+func _physics_process(delta):
 	# player movement
 	var direction = 0
 	if Input.is_action_pressed("ui_left"):
@@ -23,10 +25,12 @@ func _process(delta):
 		velocity = -Vector2.UP.rotated(rotation) * speed
 		if backwards_steering_mode == 0:
 			direction *= -1
-
-	rotation += angular_speed * direction * delta
-	position += velocity * delta
 	
+	# moves the player and causes them to slide along walls
+	move_and_slide(velocity)
+	rotation += angular_speed * direction * delta
+
+func _process(delta):
 	# player turret alway point at cursor
 	turret.look_at(get_global_mouse_position())
 	turret.rotation_degrees += 90 #either this fix or rotate sprite image
