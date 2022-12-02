8335ec2dd2454153060c86fddb461bc50b9a1089
Stefan <stefan-horst@gmx.de>
2022-12-02 05:05:39 +0100

    Cleanup

--- entities/weapons/projectile.gd
+++ entities/weapons/projectile.gd
@@ -10,18 +10,8 @@ func _physics_process(_delta):
 	if collision:
 		## reflect projectiles that hit walls
 		velocity = velocity.bounce(collision.normal)
-		print(rad2deg(velocity.angle()))
+		
 		## determine rotation of reflected projectile
-		"""# one way to measure direction: left = 0, right = pi, top = 0.5pi, bottom = -0.5pi
-		var col = collision.normal.angle()
-		var angle
-		if col > 0.25*PI and col < 0.75*PI: # top quarter circle 
-			angle = PI + rotation
-		elif col < -0.25*PI and col > -0.75*PI: # bottom quarter circle 
-			angle = rotation - PI
-		else:
-			angle = rotation
-		rotation = 2*PI - angle"""
 		# velocity.angle rotated 90 degrees
 		rotation = velocity.angle() + 0.5*PI
 		amount_bounces += 1
