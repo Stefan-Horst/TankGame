8a7adb54da7c3a914858edc013b2a2d852f54290
Stefan <stefan-horst@gmx.de>
2022-12-03 06:15:31 +0100

    Add gun cooldown

--- game.gd
+++ game.gd
@@ -1,9 +1,22 @@
 extends Node2D
 
-func _on_Player_shoot_projectile(Projectile, location, direction):
-	var p = Projectile.instance()
-	add_child(p)
-	p.position = location
-	p.rotation = direction
-	p.velocity = p.velocity.rotated(direction)
+var player_can_shoot = true
 
+onready var cooldown_timer = $Player/TimerCooldown
+
+func _on_Player_shoot_projectile(Projectile, location, direction):
+	if player_can_shoot:
+		player_can_shoot = false
+		
+		var p = Projectile.instance()
+		add_child(p)
+		
+		p.position = location
+		p.rotation = direction
+		p.velocity = p.velocity.rotated(direction)
+		
+		cooldown_timer.wait_time = p.cooldown
+		cooldown_timer.start()
+
+func _on_TimerCooldown_timeout():
+	player_can_shoot = true
