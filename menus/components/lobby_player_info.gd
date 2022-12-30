5d70a8d094dec968008df56c760aa27ccdbeb414
Stefan <stefan-horst@gmx.de>
2022-12-30 05:24:34 +0100

    Update lobby_player_info.gd

--- menus/components/lobby_player_info.gd
+++ menus/components/lobby_player_info.gd
@@ -5,6 +5,8 @@ extends PanelContainer
 const TEXT_PLAYER = "Player %s:"
 const TEXT_NAME = "Name: %s"
 
+var id = -1
+
 onready var label_player = $MarginContainer/VBoxContainer/LabelPlayer
 onready var label_name = $MarginContainer/VBoxContainer/LabelName
 onready var label_role = $MarginContainer/VBoxContainer/LabelRole
@@ -19,12 +21,28 @@ func set_player(number):
 
 
 func set_name(name_player):
+	#if not name_player.begins_with("Name: "):
 	label_name.text = TEXT_NAME % name_player
 
 
 func set_role(role):
-	label_role = role
+	label_role.text = role
+
+
+func get_name():
+	return label_name.text
+
+
+func get_role():
+	return label_role.text
 
 
 func show_connected():
 	modulate = Color("ffffff") # reset modulation
+
+
+func hide():
+	id = -1
+	set_name("")
+	set_role("")
+	_ready()
